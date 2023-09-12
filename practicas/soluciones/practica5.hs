0c08be833844435c73d22b393b0b72d29a084897
Fausto Martínez <fnmartinez@dc.uba.ar>
2023-09-11 22:44:46 -0300

    Mas ejercicios en Practica 5

--- practicas/soluciones/practica5.hs
+++ practicas/soluciones/practica5.hs
@@ -33,7 +33,7 @@ todosDistintos (x:xs) | pertenece x xs = False
                       | otherwise = todosDistintos xs
 
 hayRepetidos :: (Eq t) => [t] -> Bool
-heyRepetidos [] = False
+hayRepetidos [] = False
 hayRepetidos [x] = False
 hayRepetidos (x:xs) | pertenece x xs = True
                     | otherwise = hayRepetidos xs
@@ -45,7 +45,7 @@ quitar e (x:xs) | x == e = xs
 
 quitarTodos::(Eq t)=>t->[t]->[t] -- Quito todas las veces que aparezca el elemento
 quitarTodos e [] = []
-quitarTodos e (x:xs) | x == e = quitarTodos e xs 
+quitarTodos e (x:xs) | x == e = quitarTodos e xs
                      | otherwise = x:quitarTodos e xs
 
 eliminarRepetidos :: (Eq t) => [t] -> [t]
@@ -53,15 +53,52 @@ eliminarRepetidos [] = []
 eliminarRepetidos (x:xs) = x:eliminarRepetidos (quitarTodos x xs)
 --Voy eliminando a todos los que esten repetidos, agarrando cada elemento, y borrando todas las veces adicionales que aparezca en la lista
 
--- ! ANALIZAR CASO CON length xs != length ys
+--estaContenida verifica si cada elemento de una lista xs pertenece a otra lista ys, es decir, si xs esta contenida en ys
+estaContenida :: (Eq t) => [t] -> [t] -> Bool
+estaContenida [] ys = True
+estaContenida (x:xs) ys = pertenece x ys && estaContenida xs ys
+
 mismosElementos :: (Eq t) => [t] -> [t] -> Bool
-mismosElementos [] [] = True
-mismosElementos (x:xs) (y:ys) = pertenece x (eliminarRepetidos (y:ys)) && pertenece y (eliminarRepetidos (x:xs)) && mismosElementos xs ys
+mismosElementos xs ys = estaContenida xs ys && estaContenida ys xs
+
+capicua :: (Eq t) => [t] -> Bool
+capicua xs = xs == reverso xs
 ---------EJERCICIO 3----------
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
 maximo :: [Integer]->Integer
 maximo [x] = x
 maximo (x:y:xs) | x>y = maximo (x:xs)
                 | otherwise = maximo (y:xs)
+
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN n (x:xs) = (x+n) : sumarN n xs
+
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero xs = sumarN primero xs
+    where primero = head xs
+
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo xs = sumarN (ultimo xs) xs
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x:xs) | even x = x:pares xs
+             | otherwise = pares xs
+-- Si es par lo "concateno" en la lista, si no no
+
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN _ [] =  []
+multiplosDeN n (x:xs) | mod x n == 0 = x:multiplosDeN n (xs)
+                      | otherwise = multiplosDeN n (xs)
+-- Es analogo a 'pares', pero en lugar de fijarme modulo 2, me fijo modulo n
 
 minimo :: [Integer]->Integer --Funcion auxiliar para ordenar
 minimo [x] = x
